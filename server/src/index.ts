3e746600c9ff6c7b6a4cd588afad1be6147d03b3
Oliwia Putyra <eillomilky@gmail.com>
2023-04-05 19:22:46 +0200

    Server: Add a listening for connection in sockets

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,36 @@
-import { config } from 'dotenv';
-import http from 'http';
-import express, { Application, NextFunction, Request, Response } from 'express';
+import { config } from "dotenv";
+import http from "http";
+import express, { Application, NextFunction, Request, Response } from "express";
 import { Server } from "socket.io";
 import cors from "cors";
 
 config();
 
 const app: Application = express();
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
 
 app.use(cors());
 
-const io: Server = new Server(server, {
+const io: Server = new Server(httpServer, {
 	cors: {
 		origin: "http://localhost:3000",
-		methods: ["GET", "POST"]
+		methods: [ "GET", "POST" ]
 	}
 });
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-	res.send('Express server with TSesesss');
+io.on("connection", (socket) => {
+	console.log(`User connected ${ socket.id }`);
+	socket.on('disconnect', () => {
+		console.log('User disconnected');
+	});
+});
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+	res.send("Express server with TSesesss");
 });
 
 const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => {
-	console.log(`Server is listening on port ${PORT}`);
+httpServer.listen(PORT, () => {
+	console.log(`Server is listening on port ${ PORT }`);
 });
