eac561c86f346a9742dc81b5bef9ac8b12bd1eff
papryk <patison02@gmail.com>
2023-04-25 22:03:19 +0200

    somehow working game mechanics for two people

--- client/src/routes/GamePage.tsx
+++ client/src/routes/GamePage.tsx
@@ -7,7 +7,7 @@ import socketService from "../services/socketService";
 const GameContainer = styled.div`
     display: flex;
     flex-director: column;
-    font-family: "Zen Tokyo Zoo", cursive;
+    font-family: "arial";
     position: relative;
 `;
 
@@ -75,7 +75,130 @@ export interface IStartGame {
 }
 
 const GamePage = () => {
-	return (/*
+  const [matrix, setMatrix] = useState<IPlayMatrix>([
+    [null,null,null],
+    [null,null,null],
+    [null,null,null],
+  ]);
+
+  const {
+    playerSymbol,
+    setPlayerSymbol,
+    setPlayerTurn,
+    isPlayerTurn,
+    setGameStarted,
+    isGameStarted,
+  } = useContext(gameContext);
+
+  const checkGameState = (matrix: IPlayMatrix) => {
+    for (let i = 0; i < matrix.length; i++){
+      let row = [];
+      for(let j = 0; j < matrix[i].length; j++){
+        row.push(matrix[i][j]);
+      }
+
+      if (row.every((value) => value && value === playerSymbol)){
+        return [true, false];
+      }
+      else if (row.every((value) => value && value !== playerSymbol)){
+        return [false, true];
+      }
+    }
+
+    for (let i = 0; i < matrix.length; i++){
+      let column = [];
+      for (let j = 0; j < matrix[i].length; j++){
+        column.push(matrix[j][i]);
+      }
+      if(column.every((value) => value && value === playerSymbol)){
+        return [true, false];
+      }
+      else if(column.every((value) => value && value !== playerSymbol)){
+        return [false,true];
+      }
+    }
+    if(matrix[1][1]){
+      if(matrix[0][0] === matrix[1][1] && matrix[2][2] === matrix[1][1]){
+        if(matrix[1][1] === playerSymbol){
+          return [true, false];
+        }
+        else return [false,true];
+      }
+
+      if(matrix[2][0] === matrix[1][1] && matrix[0][2] === matrix[1][1]){
+        if(matrix[1][1] === playerSymbol){
+          return [true, false];
+        }
+        else return [false, true];
+      }
+    }
+
+    if(matrix.every((m) => m.every((v) => v !== null))) {
+      return [true, true];
+    }
+    return [false, false];
+  };
+
+  const updateGameMatrix = (column: number, row: number, symbol: "x" | "o") => {
+    const newMatrix = [...matrix];
+
+    if(newMatrix[row][column] === null || newMatrix[row][column] === "null"){
+      newMatrix[row][column] = symbol;
+      setMatrix(newMatrix);
+    }
+
+    if(socketService.socket){
+      gameService.updateGame(socketService.socket, newMatrix);
+      const [currentPlayerWon, otherPlayerWon] = checkGameState(newMatrix);
+      if(currentPlayerWon && otherPlayerWon){
+        gameService.gameWin(socketService.socket, "The Game is a TIE!");
+        alert("The game is a TIE!");
+      }
+      else if (currentPlayerWon && !otherPlayerWon){
+        gameService.gameWin(socketService.socket, "You Lost!");
+        alert("You Won!");
+      }
+      setPlayerTurn(false);
+    }
+  };
+
+  const handleGameUpdate = () => {
+    if(socketService.socket)
+      gameService.onGameUpdate(socketService.socket, (newMatrix) => {
+        setMatrix(newMatrix);
+        checkGameState(newMatrix);
+        setPlayerTurn(true);
+      });
+  };
+
+  const handleGameStart = () => {
+    if(socketService.socket)
+      gameService.onStartGame(socketService.socket, (options) => {
+        setGameStarted(true);
+        setPlayerSymbol(options.symbol);
+        if (options.start) setPlayerTurn(true);
+        else setPlayerTurn(false);
+      });
+  };
+
+  const handleGameWin = () => {
+    if (socketService.socket)
+      gameService.OnGameWin(socketService.socket, (message) => {
+        console.log("Here", message);
+        setPlayerTurn(false);
+        alert(message);
+      });
+  };
+
+  useEffect(() => {
+    handleGameUpdate();
+    handleGameStart();
+    handleGameWin();
+  }, []);
+//}
+
+//const GamePage = () => {
+	return (
 		<GameContainer>
       {!isGameStarted && (
         <h2>Waiting for Other Player to Join to Start the Game!</h2>
@@ -106,8 +229,7 @@ const GamePage = () => {
           </RowContainer>
         );
       })}
-    </GameContainer>*/
-    <div>hello world!</div>
+    </GameContainer>
 	);
 }
 
